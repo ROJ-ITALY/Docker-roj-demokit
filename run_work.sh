2b072b19fbdfa4f91b1ff993f75c441c64fd98a1
Stefano Gurrieri <stefano.gurrieri@roj.com>
2019-09-20 14:53:52 +0200

    - Add example how to run a docker yocto container.

---
+++ run_work.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This is an example on how to run a docker yocto container.
+docker run \
+                --rm \
+                -it \
+                -v "$(pwd)/work:/tmp/work" \
+                -v "$(pwd)/../shared/cache:/tmp/cache" \
+                -v "$(pwd)/sources:/tmp/sources" \
+                -v "$(pwd)/../shared/downloads:/tmp/downloads" \
+                yocto-roj-demokit \
+                --id="$(id -u):$(id -g)" \
+                --work="/tmp/work" \
+                --download="/tmp/downloads" \
+                --source="/tmp/sources" \
+                --cache="/tmp/cache" \
+                "$@"
